47be188f8f52f61c375d75042bc35d3227fa931f
David Conner <dconner.pro@gmail.com>
2013-09-11 12:25:22 -0600

    added some utils to help with testing

--- src/handwritten_digits/core.clj
+++ src/handwritten_digits/core.clj
@@ -1,15 +1,26 @@
 (ns handwritten-digits.core)
+
+(ns handwritten-digits.util)
+
+(defn make-byte-array [s] 
+  (byte-array (map #(byte %) s)))
+
+(defn byte-to-signed [b]
+  (- b 128))
+
+(defn make-unsigned-byte-array [s]
+  (map byte-to-signed (make-byte-array s))
  
 (ns handwritten-digits.mnist-digits
   (:use [gloss core io]
-        [incanter core stats charts]
+        [incanter core stats charts])
 
   (def mnist-images-magic-num 2051)
   (def mnist-labels-magic-num 2049)
 
   ;;(defcodec mnist-images-header (enum :uint32-be))
 
-))
+)
 
 ;; reading mnist 
 ;; - http://csi701-group2.googlecode.com/svn-history/r13/trunk/populate-db/clojure/src/populate_db_clojure/core.clj
--- test/handwritten_digits/test/core.clj
+++ test/handwritten_digits/test/core.clj
@@ -3,6 +3,12 @@
             [handwritten-digits.core :refer :all]))
 
 (ns handwritten-digits.test.mnist-digits
-  (:use [handwritten-digits.mnist-digits]))
+  (:use [handwritten-digits mnist-digits util]
+
+  (def mnist-images-magic-num-byte-arr '(0x00 0x00 0x80 0x03))
+  (def mnist-labels-magic-num-byte-arr '(0x00 0x00 0x80 0x01))
+
+  
+))
 
 
