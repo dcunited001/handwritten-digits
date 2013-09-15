28dab4da01642dc2b6b5428199c97e5c21703475
David Conner <dconner.pro@gmail.com>
2013-09-15 03:42:49 -0600

    notes: moved links to project.clj

--- src/handwritten_digits/core.clj
+++ src/handwritten_digits/core.clj
@@ -9,25 +9,3 @@
     (io/copy input output)
     (.toByteArray output)))
  
-;; reading mnist 
-;; - http://csi701-group2.googlecode.com/svn-history/r13/trunk/populate-db/clojure/src/populate_db_clojure/core.clj
-
-;; clojuresphere
-;; - gloss    http://www.clojuresphere.com/gloss/gloss
-;; - incanter http://www.clojuresphere.com/incanter/incanter
-
-;; incanter api
-;; - http://liebke.github.io/incanter/branch-master/index.html
-
-;; incanter examples (matrices & statistics)
-;; - https://github.com/liebke/incanter/wiki/Matrices
-;; - https://github.com/liebke/incanter/wiki/Statistics-Examples
-
-;; incanter cheatsheet
-;; - http://incanter.org/docs/incanter-cheat-sheet.pdf
-
-;; gloss examples
-;; - opentoken https://github.com/mch/opentoken
-;; - post   http://comments.gmane.org/gmane.comp.java.clojure.aleph/21
-;; - codecs https://github.com/andrewvc/engulf/blob/master/src/engulf/comm/netchan.clj
-;; - tests  https://github.com/andrewvc/engulf/blob/master/test/engulf/test/comm/netchan_test.clj
