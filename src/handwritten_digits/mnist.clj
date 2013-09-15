f586d29ba48c42ab04e6dbf562c04508366776be
David Conner <dconner.pro@gmail.com>
2013-09-15 04:33:09 -0600

    mnist: can now decode labels data

--- src/handwritten_digits/mnist.clj
+++ src/handwritten_digits/mnist.clj
@@ -7,22 +7,23 @@
 (def mnist-images-magic-num 2051)
 (def mnist-labels-magic-num 2049)
 
-(defcodec mnist-label-header (ordered-map :magic :uint32-be 
-                                          :num :uint32-be))
+(defcodec mnist-label-header 
+  (ordered-map :magic :uint32-be 
+               :num :uint32-be))
 
-(defcodec mnist-image-header (ordered-map :magic :uint32-be
-                                          :num :uint32-be
-                                          :rows :uint32-be
-                                          :cols :uint32-be))
+(defn mnist-label-header->body [head]
+  (compile-frame (ordered-map :labels (repeated :byte :prefix :none))))
 
-(defcodec mnist-labels
-  (let [
-        h->b (fn [head] (finite-frame (:num head) (ordered-map :label :ubyte)))
-        b->h (fn [body] (ordered-map :magic mnist-labels-magic-num
-                                     :num (count body)))
-        ]
-    (header (ordered-map :magic (enum :uint32-be mnist-labels-magic-num)
-                         :num :uint32-be) h->b b->h)))
+(defn mnist-label-body->header [body] 
+  (ordered-map :magic mnist-labels-magic-num 
+               :num (count body)))
 
-;;(defcodec mnist-labels (compile-frame mnist-label-header))
-;;(defcodec mnist-images-header (enum :uint32-be))
+(defcodec mnist-labels-codec
+  (header mnist-label-header mnist-label-header->body mnist-label-body->header))
+
+
+(defcodec mnist-image-header 
+  (ordered-map :magic :uint32-be
+               :num :uint32-be
+               :rows :uint32-be
+               :cols :uint32-be))
--- test/handwritten_digits/test/mnist.clj
+++ test/handwritten_digits/test/mnist.clj
@@ -9,6 +9,17 @@
 (def image-bytes (read-bytes "test/data/images-data"))
 (def image-header-bytes (read-bytes "test/data/images-header"))
 
+(deftest label-header-encode-test
+  (let [magic mnist-labels-magic-num
+        n 2
+        encoded (gloss.io/encode mnist-label-header {:magic magic :num n})
+        bytes label-header-bytes]
+    ;; TODO: figure out interface to object returned by gloss.io/encode 
+    ;; (gloss.data.bytes.core.SingleBufferSequence or HeapByteBuffer)
+    ;;(is (= (first encoded) (first bytes)))
+    ;;(is (= (last encoded) (last bytes)))
+    ))
+
 (deftest label-header-decode-test 
   (let [decoded (gloss.io/decode mnist-label-header label-header-bytes)
         magic mnist-labels-magic-num
@@ -36,15 +47,8 @@
 ;;   (is (= (encoded :labels) '(1 2 3 4)))
 ;;   ))
 
-;; (deftest labels-data-decode-test
-;;   (let [decoded (gloss.io/decode
-;;                  mnist-label-header 
-;;                  label-bytes)]
-;;     (is (= (decoded :labels) '(1 2 3 4)))))
+(deftest labels-data-decode-test
+  (let [decoded (gloss.io/decode mnist-labels-codec label-bytes)]
+    (is (= (decoded :labels) '(1 2 3 4)))))
 
-;; (deftest a-test 
-;;   (prn (map str label-bytes)))
 
-;; (deftest b-test
-;;   (is (= (gloss.io/decode mnist-label-header label-bytes)))
-;;   )
