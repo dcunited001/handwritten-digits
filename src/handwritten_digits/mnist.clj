58f72345b06e908a55ff9f98ed65d51f9d364c3d
David Conner <dconner.pro@gmail.com>
2013-09-15 03:44:57 -0600

    mnist: broke problem into smallest possible pieces lol
    * now decoding just the headers for labels/images

--- src/handwritten_digits/mnist.clj
+++ src/handwritten_digits/mnist.clj
@@ -1,23 +1,28 @@
 (ns handwritten-digits.mnist
   (:use [gloss core io]
-         ;;[incanter core stats charts]
-         ))
+        ;;[incanter core stats charts]
+        ))
 
-  (def mnist-images-magic-num 2051)
-  (def mnist-labels-magic-num 2049)
+;; TODO: change header to validate magic-numbers
+(def mnist-images-magic-num 2051)
+(def mnist-labels-magic-num 2049)
 
-  (defn mnist-magic-num-header [magic] (enum :uint32-be {:hdr magic}))
+(defcodec mnist-label-header (ordered-map :magic :uint32-be 
+                                          :num :uint32-be))
 
-  (defn mnist-label-header []
-    (let [
-          h->b (fn [head] (compile-frame 
-                           (finite-frame (head :num)
-                                         [:label :ubyte])))
-          b->h (fn [body] [mnist-labels-magic-num
-                           (count body)])
-          ]
-    (header (compile-frame [:magic (mnist-magic-num-header mnist-labels-magic-num)
-                            :num :uint32-be]) h->b b->h)))
+(defcodec mnist-image-header (ordered-map :magic :uint32-be
+                                          :num :uint32-be
+                                          :rows :uint32-be
+                                          :cols :uint32-be))
 
-  ;;(defcodec mnist-labels (compile-frame mnist-label-header))
-  ;;(defcodec mnist-images-header (enum :uint32-be))
+(defcodec mnist-labels
+  (let [
+        h->b (fn [head] (finite-frame (:num head) (ordered-map :label :ubyte)))
+        b->h (fn [body] (ordered-map :magic mnist-labels-magic-num
+                                     :num (count body)))
+        ]
+    (header (ordered-map :magic (enum :uint32-be mnist-labels-magic-num)
+                         :num :uint32-be) h->b b->h)))
+
+;;(defcodec mnist-labels (compile-frame mnist-label-header))
+;;(defcodec mnist-images-header (enum :uint32-be))
--- test/handwritten_digits/test/mnist.clj
+++ test/handwritten_digits/test/mnist.clj
@@ -3,45 +3,48 @@
             [gloss core io])
   (:use [handwritten-digits mnist util]))
 
-  (defn flatten-byte-arrays [data keys] 
-    (-> (reduce #(conj %1 (data %2)) '() keys) 
-        flatten 
-        make-unchecked-byte-array))
+(def label-bytes (read-bytes "test/data/labels-data"))
+(def label-header-bytes (read-bytes "test/data/labels-header"))
 
-  (def labels-data {:magic '(0x00 0x00 0x80 0x01)
-                    :num '(0x00 0x00 0x00 0x04)
-                    :labels '(0x01 0x02 0x03 0x04)})
+(def image-bytes (read-bytes "test/data/images-data"))
+(def image-header-bytes (read-bytes "test/data/images-header"))
 
-  (def images-data {:magic '(0x00 0x00 0x80 0x03)
-                    :num '(0x00 0x00 0x00 0x02)
-                    :rows '(0x00 0x00 0x00 0x04)
-                    :cols '(0x00 0x00 0x00 0x04)
-                    :image1 '(0x00 0x22 0xEE 0x00
-                              0x00 0xEE 0xEE 0x00
-                              0x00 0x11 0xEE 0x00
-                              0x00 0x11 0xEE 0x00)
-                    :image2 '(0x00 0x22 0xEE 0x00
-                              0x00 0xEE 0xEE 0x00
-                              0x00 0x11 0xEE 0x00
-                              0x00 0x11 0xEE 0x00)
-                    })
+(deftest label-header-decode-test 
+  (let [decoded (gloss.io/decode mnist-label-header label-header-bytes)
+        magic mnist-labels-magic-num
+        n 4]
+    (is (= (decoded :num) n))
+    (is (= (decoded :magic) magic))
+    ))
 
-  (def label-bytes (read-bytes "test/data/labels-data"))
-  (def image-bytes (read-bytes "test/data/images-data"))
+(deftest image-header-decode-test
+  (let [decoded (gloss.io/decode mnist-image-header image-header-bytes)
+        magic mnist-images-magic-num
+        n 2
+        r 4
+        c 4]
+    (is (= (decoded :magic) magic))
+    (is (= (decoded :num) n))
+    (is (= (decoded :rows) r))
+    (is (= (decoded :cols) c))
+    ))
 
-  ;; FIXME: y u no defcodec
-  ;; (deftest labels-data-encode-test 
-  ;;   (let [labels '(1 2 3 4)
-  ;;         encoded (gloss.io/encode mnist-label-header labels)]
-  ;;   (is (= (encoded :labels) '(1 2 3 4)))
-  ;;   ))
+;; ;; FIXME: y u no defcodec
+;; (deftest labels-data-encode-test 
+;;   (let [labels '(1 2 3 4)
+;;         encoded (gloss.io/encode mnist-label-header labels)]
+;;   (is (= (encoded :labels) '(1 2 3 4)))
+;;   ))
 
-  (deftest labels-data-decode-test
-    (let [decoded (gloss.io/decode
-                   mnist-label-header 
-                   label-bytes)]
-      (is (= (decoded :labels) '(1 2 3 4)))))
-    
-  (deftest a-test 
-    (prn (map str label-bytes)))
+;; (deftest labels-data-decode-test
+;;   (let [decoded (gloss.io/decode
+;;                  mnist-label-header 
+;;                  label-bytes)]
+;;     (is (= (decoded :labels) '(1 2 3 4)))))
 
+;; (deftest a-test 
+;;   (prn (map str label-bytes)))
+
+;; (deftest b-test
+;;   (is (= (gloss.io/decode mnist-label-header label-bytes)))
+;;   )
