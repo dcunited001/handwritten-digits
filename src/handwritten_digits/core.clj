7a8bf81a02a6e075988e9978d95703a31466927c
David Conner <dconner.pro@gmail.com>
2013-10-01 18:35:15 -0600

    core: added helpers for reading data, but they blow up
    * it uses too much memory!

--- src/handwritten_digits/core.clj
+++ src/handwritten_digits/core.clj
@@ -1,11 +1,29 @@
 (ns handwritten-digits.core)
 
 (ns handwritten-digits.util
-  (:use [clojure.java.io :as io]))
+  (:use [handwritten-digits.mnist :as mnist]
+        [incanter.core])
+  (:require [gloss.io]
+            [clojure.java.io :as io]))
 
 (defn read-bytes [file] 
   (with-open [input (io/input-stream file)
               output (new java.io.ByteArrayOutputStream)]
     (io/copy input output)
     (.toByteArray output)))
- 
+
+(defn read-labels [file] (gloss.io/decode label-codec (read-bytes file)))
+(defn read-images [file] (gloss.io/decode image-codec (read-bytes file)))
+
+(defn t-labels []
+  (let [labels (:labels (read-labels "data/train-labels-idx1-ubyte"))]
+    (to-dataset (range 1 (count labels)) labels)))
+
+(defn t-images []
+  (let [images (:images (read-images "data/train-images-idx3-ubyte"))]
+    (to-dataset (range 1 (count images)) images)))
+
+;; pass training function in to generic training alg?
+
+(defn view-labels [labels] (with-data labels (view $data)))
+
