1524a7221e4104e8a68fd24d7d0e12089685e85e
David Conner <dconner.pro@gmail.com>
2013-10-03 02:31:48 -0600

    worked on get-digit and draw-digits method, but i'm going to have to use images =/

--- src/handwritten_digits/draw.clj
+++ src/handwritten_digits/draw.clj
@@ -1,17 +1,38 @@
 (ns handwritten-digits.draw
-  (:use [incanter core charts]))
+  (:use [incanter core charts])
+  (:gen-class))
 
-(defn draw-digits [data x-size y-size nx ny]
-  (let [xmax (* x-size nx) 
-        ymax (* y-size ny)]
-  (heat-map get-pixel 0 0 xmax ymax
-            :title "digits" 
-            :color false)))
+(declare ^:dynamic sizex)
+(declare ^:dynamic sizey)
+(declare ^:dynamic nx)
+(declare ^:dynamic ny)
+(declare ^:dynamic data)
 
 (defn get-pixel [x y]
   ;; translate 2-d coordinates 
   ;;   to retrieve from list of unwrapped matrices
-  (let [n (+ (quot x size-x) (* nx (quot y size-y)))
-        m (+ (mod  x size-x) (* size-x (mod y size-y)))]
-    (sel data n m)))
+  (let [n (+ (quot x sizex) (* nx (quot y sizey)))
+        m (+ (mod  x sizex) (* sizex (mod y sizey)))]
+    (prn (str "X=" x " Y=" y " M=" m " N=" n))
+    (sel data m n)))
+
+(defn draw-digits [data sizex sizey nx ny]
+  (let [xmax (- (* sizex nx) 1)  
+        ymax (- (* sizey ny) 1)]
+    (binding [sizex sizex
+              sizey sizey
+              nx nx
+              ny ny
+              data data]
+      (heat-map get-pixel
+                0 xmax 0 ymax
+                :title "digits" 
+                :color false))))
+
+  ;; (heat-map get-pixel 0 0 xmax ymax
+  ;;           :title "digits" 
+  ;;           :color false)))
+
+(defn draw-100-digits [data] 
+  (draw-digits (to-matrix (to-dataset (take 100 data))) 28 28 10 10))
 
