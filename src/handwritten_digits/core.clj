6cb202f0850a7e7dca16656860f945f06234d561
David Conner <dconner.pro@gmail.com>
2013-10-03 01:02:38 -0600

    moved util namespace to its own file

--- src/handwritten_digits/core.clj
+++ src/handwritten_digits/core.clj
@@ -1,29 +1,15 @@
-(ns handwritten-digits.core)
+(ns handwritten-digits.core
+  (:require [handwritten-digits.util :as util]
+            [handwritten-digits.mnist :as mnist]
+            [handwritten-digits.net :as net]
+            [handwritten-digits.draw :as draw])
+  (:gen-class :main true))
 
-(ns handwritten-digits.util
-  (:use [handwritten-digits.mnist :as mnist]
-        [incanter.core])
-  (:require [gloss.io]
-            [clojure.java.io :as io]))
+(defn -main [& args]
+  (let [labels (:labels (util/read-labels "data/train-labels-idx1-ubyte"))
+        images (:images (util/read-images "data/train-images-idx3-ubyte"))]
+    (println args)
+    (draw/draw-100-digits (to-matrix (to-dataset (take 100 images))))
 
-(defn read-bytes [file] 
-  (with-open [input (io/input-stream file)
-              output (new java.io.ByteArrayOutputStream)]
-    (io/copy input output)
-    (.toByteArray output)))
-
-(defn read-labels [file] (gloss.io/decode label-codec (read-bytes file)))
-(defn read-images [file] (gloss.io/decode image-codec (read-bytes file)))
-
-(defn t-labels []
-  (let [labels (:labels (read-labels "data/train-labels-idx1-ubyte"))]
-    (to-dataset (range 1 (count labels)) labels)))
-
-(defn t-images []
-  (let [images (:images (read-images "data/train-images-idx3-ubyte"))]
-    (to-dataset (range 1 (count images)) images)))
-
-;; pass training function in to generic training alg?
-
-(defn view-labels [labels] (with-data labels (view $data)))
-
+)   
+)
---
+++ src/handwritten_digits/util.clj
@@ -0,0 +1,26 @@
+(ns handwritten-digits.util
+  (:use [handwritten-digits.mnist :as mnist]
+        [incanter.core])
+  (:require [gloss.io]
+            [clojure.java.io :as io]))
+
+(defn read-bytes [file] 
+  (with-open [input (io/input-stream file)
+              output (new java.io.ByteArrayOutputStream)]
+    (io/copy input output)
+    (.toByteArray output)))
+
+(defn read-labels [file] (gloss.io/decode label-codec (read-bytes file)))
+(defn read-images [file] (gloss.io/decode image-codec (read-bytes file)))
+
+(defn t-labels []
+  (let [labels (:labels (read-labels "data/train-labels-idx1-ubyte"))]
+    (to-matrix (to-dataset labels))))
+
+(defn t-images []
+  (let [images (:images (read-images "data/train-images-idx3-ubyte"))]
+    (to-matrix (to-dataset images))))
+
+;; pass training function in to generic training alg?
+
+(defn view-labels [labels] (with-data labels (view $data)))
