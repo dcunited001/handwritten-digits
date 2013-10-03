6d81b62aacc840a16a5f0cdd6a5432a0bab11409
David Conner <dconner.pro@gmail.com>
2013-10-03 02:30:49 -0600

    worked on -main method.  heatmap isn't working and isn't going to work grrrr

--- src/handwritten_digits/core.clj
+++ src/handwritten_digits/core.clj
@@ -2,14 +2,17 @@
   (:require [handwritten-digits.util :as util]
             [handwritten-digits.mnist :as mnist]
             [handwritten-digits.net :as net]
-            [handwritten-digits.draw :as draw])
+            [handwritten-digits.draw :as draw]
+            [incanter.core :as inc])
   (:gen-class :main true))
 
 (defn -main [& args]
   (let [labels (:labels (util/read-labels "data/train-labels-idx1-ubyte"))
         images (:images (util/read-images "data/train-images-idx3-ubyte"))]
     (println args)
-    (draw/draw-100-digits (to-matrix (to-dataset (take 100 images))))
+    (println (count (take 5 images)))
+    (println (inc/to-matrix (inc/to-dataset (take 5 images))))
+    (println (inc/sel (inc/to-matrix (inc/to-dataset (take 5 images))) :cols 0))
 
-)   
-)
+    (draw/draw-100-digits images)
+))
