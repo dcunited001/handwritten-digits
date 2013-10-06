d6c6485009229486dd825b7123dd5dee56eec72f
David Conner <dconner.pro@gmail.com>
2013-10-06 03:09:41 -0600

    draw test: testing negative values
    * i guess this works because the bits just wrap
    
    * values are supposed to be 0-255

--- test/digits/test/draw.clj
+++ test/digits/test/draw.clj
@@ -3,7 +3,7 @@
   (:use [digits draw]))
 
 (deftest digit-image-test
-  (let [data [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15]
+  (let [data [0 -1 2 -3 4 5 6 7 8 9 10 11 12 13 14 15]
         sizex 4
         sizey 4]
     (digit-image data sizex sizey))
