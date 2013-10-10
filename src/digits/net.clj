ac75a3f64f8d154052cea19ec081a2c1d6c7d0f0
David Conner <dconner.pro@gmail.com>
2013-10-10 02:08:53 -0600

    net: wrote labels-to-binary-matrix

--- src/digits/net.clj
+++ src/digits/net.clj
@@ -22,15 +22,6 @@
 (defn init-theta [nr nc mu sigma]
   (m/rnorm mu sigma nr (+ 1 nc)))
 
-(defn convert-to-img-seq 
-  "Preps a theta matrix for printing 
-    by removing the bias unit 
-    and scaling all values to 0-255"
-  [mat]
-  ;; FIXME: scale with (* 255 (sigmoid mat))
-  (m/as-vec (remove-bias-unit mat))
-)
-
 (defn sigmoid 
   "Runs the sigmoid function against values in the matrix"
   [z]
@@ -42,11 +33,29 @@
   [z]
   (m/mult z (m/- 1 z)))
 
+(defn convert-to-img-seq 
+  "Preps a theta matrix for printing 
+    by removing the bias unit 
+    and scaling all values to 0-255"
+  [mat]
+  ;; FIXME: scale with (* 255 (sigmoid mat))
+  (m/as-vec (remove-bias-unit (m/* 255.0 (sigmoid mat))))
+)
+
+(defn labels-to-binary-matrix [num-labels lbls]
+  (let [one-to-k (m/+ 1 (m/zeros (m/nrows lbls) num-labels))]
+    (prn one-to-k)
+    (prn lbls)
+    (m/map-indexed (fn [i j v] 
+                     (if (= j (int (m/get lbls i 0))) 
+                       1 0))
+                   one-to-k)))
+
 (defn process-net
   "Returns a function that can process data
     m - batch size
     img-atoms references of image atoms to update"
-  [batch-size sizex sizey nx ny num-processed-atom
+  [batch-size num-labels sizex sizey nx ny num-processed-atom
    & img-atoms]
   
   (fn process [lambda img lbl t1 t2]
@@ -54,15 +63,34 @@
       (let [remaining (count lbl)
             input (take batch-size img)
             X (add-bias-unit (m/matrix input))
-            y (take 100 (m/matrix (take batch-size lbl)))]
+            y (take 100 (m/matrix (take batch-size lbl)))
+            Y (labels-to-binary-matrix num-labels y)
+
+            ;;K (m/matrix (range 0 num-labels))
+            ;; translate y into binary matrix
+            ;;one-to-k (m/zeros batch-size num-labels)
+            ;;Y (m/map-indexed one-to-k #(if (= %2 (m/get y %1 1))))
+            ]
         ;; currently each iteration takes between 20-30 ms (just init/draw input/theta1)
         (do (reset! (first img-atoms) (draw/digits-image input sizex sizey nx ny))
             (reset! (second img-atoms) (draw/digits-image (convert-to-img-seq t1) sizex sizey 5 5))
             (reset! (nth img-atoms 2) (draw/digits-image (convert-to-img-seq t2) 5 5 5 5))
 
-            (-> (java.util.Date.) prn)
-            (prn @num-processed-atom)
+            (let [z2 (m/* X (m/t t1))
+                  a2 (add-bias-unit (sigmoid z2))
+                  z3 (m/* a2 (m/t t2))
+                  a3 (sigmoid z3)
+                  ones (m/+ 1 (m/zeros num-labels 1))
+                  
+                  ;;cost (m/div () batch-size)
+                  ])
+     
+;; cost = (-sum((Y .* log(A3')) * Ones) - sum(((1-Y) .* log(1-A3')) * Ones))/m;
+
             (swap! num-processed-atom + batch-size)
+            
+            ;;(-> (java.util.Date.) prn)
+            ;;(prn @num-processed-atom)
             (recur lambda (drop 100 img) (drop 100 lbl) t1 t2))
         )
       )
